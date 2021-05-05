91140159e7509d0876807efd0ab45f769288f649
Dhiren Brahmbhatt <dhiren.b94@gmail.com>
2021-05-05 16:32:18 +0100

    Lab done - no styling

--- src/App.js
+++ src/App.js
@@ -7,18 +7,52 @@ class App extends React.Component {
     contacts: contactsFromJSON.slice(0, 5),
   };
 
+  addRandomActor = () => {
+    let newActor = contactsFromJSON[Math.floor(Math.random()*contactsFromJSON.length)];
+    this.setState({
+      contacts : this.state.contacts.concat(newActor)
+    });
+  };
+
+  sortPopularity = (popularity) => {
+    this.setState({
+      contacts: this.state.contacts.sort((a, b) => b.popularity - a.popularity)
+    });
+  };
+
+  sortAlphabetically = (name) => {
+    this.setState({
+      contacts: this.state.contacts.sort(function (x, y) {
+        let a = x.name.toUpperCase(),
+            b = y.name.toUpperCase();
+        return a == b ? 0 : a > b ? 1 : -1;
+      })
+    });
+  };
+
+  deleteActor = (id) => {
+    this.setState({
+      contacts: this.state.contacts.filter((contact) => contact.id !== id),
+    });
+  };
+
+
   render() {
     const { contacts } = this.state;
 
     return (
       <div className="App">
         <h1>IronContacts</h1>
+        <button onClick={this.addRandomActor}>Add a Random Actor</button>
+        <button onClick={() => this.sortAlphabetically(contacts.name)}>Sort by Name</button>
+        <button onClick={() => this.sortPopularity(contacts.popularity)}>Sort by Popularity</button>
         <table>
           <thead>
             <tr>
               <th>Picture</th>
               <th>Name</th>
               <th>Popularity</th>
+              <th>Action</th>
             </tr>
           </thead>
           <tbody>
@@ -28,8 +62,9 @@ class App extends React.Component {
                   <td>
                     <img src={contact.pictureUrl} className="image" />
                   </td>
-                  <td>{contact.name}</td>
-                  <td>{contact.popularity}</td>
+                    <td>{contact.name}</td>
+                    <td>{contact.popularity}</td>
+                    <td><button onClick={() => this.deleteActor(contact.id)}>Delete Actor</button></td>
                 </tr>
               );
             })}
